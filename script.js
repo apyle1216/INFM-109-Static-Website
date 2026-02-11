f5795996aeb442e4daf0ef5eb47c1f260f8c9307
apyle1216 <apyle27@ivytech.edu>
2026-02-11 12:34:34 -0500

    Add files via upload

--- script.js
+++ script.js
@@ -6,7 +6,8 @@ const messageElement = document.getElementById('message');
 // Array of friendly courseings
 const courses = [
     "INFM 109",
-    "SDEV 120"
+    "SDEV 120",
+    "DBMS 110"
 ];
 
 // Click event handler
